bed945a199ab07416fccf71d9e29018e1cd4ce74
Tim Halle <38154184+HalleTim@users.noreply.github.com>
2023-11-11 09:08:50 +0000

    Hallo

--- backend/cypress/e2e/todo.cy.js
+++ backend/cypress/e2e/todo.cy.js
@@ -5,3 +5,12 @@ describe('ToDo Anwendung', function () {
         cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
     })
 })
+
+//test sucessfully rendered
+describe('ToDo Anwendung', function () {
+    it('successfully loads', function () {
+        cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
+        cy.contains('ToDo Anwendung')
+        cy.get('#todo').type('')
+        })
+})
