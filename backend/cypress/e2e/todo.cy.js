47f9414611f1611280813d318ea2748ebe340424
Tim Halle <38154184+HalleTim@users.noreply.github.com>
2023-11-11 10:29:25 +0000

    Tests123422

--- backend/cypress/e2e/todo.cy.js
+++ backend/cypress/e2e/todo.cy.js
@@ -1,5 +1,10 @@
 const PORT = process.env.PORT || 3000;
 
+const headers = {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+};
+
 describe('ToDo Anwendung', function () {
     it('successfully loads', function () {
         cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
@@ -10,7 +15,47 @@ describe('ToDo Anwendung', function () {
 describe('ToDo Anwendung', function () {
     it('successfully loads', function () {
         cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
-        cy.contains('ToDo Anwendung')
-        cy.get('#todo').type('')
+        cy.contains('Todo Liste')
+        //date field contains 11/14/2023
+        cy.get('#due').should('have.value', '2023-11-14')
+        cy.get('#todo').should('have.value', '')
+        cy.get('#status').should('have.value', '0')
         })
+})
+
+//test add todo
+describe('ToDo Anwendung', function () {
+    it('add', function () {
+        cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
+        cy.get('#todo').type('test todo')
+        cy.get('#due').type('2023-11-14')
+        cy.get('#status').select('0')
+        cy.get('#add').click()
+        cy.get('#todo').should('have.value', '')
+        cy.get('#due').should('have.value', '2023-11-14')
+        cy.get('#status').should('have.value', '0')
+        cy.contains('test todo')
+    })
+})
+
+//test delete todo
+describe('ToDo Anwendung', function () {
+    it('delete', function () {
+        cy.request({
+            method: 'POST', 
+            url: 'http://localhost:3000/todos',
+            headers: headers,
+            body: {
+                "title": "12345",
+                "due": "2023-11-14T00:00:00.000Z",
+                "status": 0
+            }  
+        }).then((response) => {
+            //get todo id
+            const id = response.body._id
+            cy.visit(`http://localhost:${PORT}/todo.html`)
+            cy.get(`#todo-${id}`).find('Löschen').trigger("click")
+            cy.contains(id).should('not.exist')
+        })
+    })
 })
