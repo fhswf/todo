80a5eff230b779ac952c3058411a67e7000d82ae
florianw830 <florianw830@gmail.com>
2024-02-17 20:16:08 +0100

    cypress initialisation

---
+++ frontend/cypress/e2e/todo-frontend-tests.cy.js
@@ -0,0 +1,16 @@
+describe('Todo List', () => {
+    it('Adds a new todo', () => {
+     cy.visit('/todo.html'); // Assuming your todo app is served on the root path
+     it('loads the initial page', () => {
+      cy.contains('h1', 'Todo Liste')
+    })
+
+    it('allows users to add a new todo', () => {
+      cy.get('#todo').type('Neues Todo von Cypress')
+      cy.get('#due').type('2025-01-01')
+      cy.get('#status').select('offen')
+      cy.get('form').submit()
+      cy.contains('.todo .title', 'Neues Todo von Cypress')
+  })
+});
+});
