ab4ffb45d01a2ea3cebd1277cc0a578fa111e41c
Tim Halle <38154184+HalleTim@users.noreply.github.com>
2024-02-19 18:00:29 +0000

    Ganz viele Tests

--- backend/cypress/e2e/todo.cy.js
+++ backend/cypress/e2e/todo.cy.js
@@ -13,11 +13,16 @@ describe('ToDo Anwendung', function () {
 
 //test sucessfully rendered
 describe('ToDo Anwendung', function () {
-    it('successfully loads', function () {
+    it('Lade Startseite', function () {
         cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
         cy.contains('Todo Liste')
-        //date field contains 11/14/2023
-        cy.get('#due').should('have.value', '2023-11-14')
+
+        let date=new Date(Date.now() + 3 * 86400000)
+        let year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+        let month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
+        let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+        cy.get('#due').should('have.value', `${year}-${month}-${day}`)
+
         cy.get('#todo').should('have.value', '')
         cy.get('#status').should('have.value', '0')
         })
@@ -25,28 +30,42 @@ describe('ToDo Anwendung', function () {
 
 //test add todo
 describe('ToDo Anwendung', function () {
-    it('add', function () {
+    it('Füge ToDo hinzu', function () {
+        let lengthBefore=0
         cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
+        cy.get('#todo-list')
+            .its('length')
+            .then((length) => {
+                lengthBefore = length
+}           );
         cy.get('#todo').type('test todo')
         cy.get('#due').type('2023-11-14')
         cy.get('#status').select('0')
         cy.get('#add').click()
+        cy.get('#todo-list').should('have.length', lengthBefore+1);
+
+
+
+        
         cy.get('#todo').should('have.value', '')
-        cy.get('#due').should('have.value', '2023-11-14')
+        let date=new Date(Date.now() + 3 * 86400000)
+        let year = new Intl.DateTimeFormat('en', { year: 'numeric' }).format(date);
+        let month = new Intl.DateTimeFormat('en', { month: '2-digit' }).format(date);
+        let day = new Intl.DateTimeFormat('en', { day: '2-digit' }).format(date);
+        cy.get('#due').should('have.value', `${year}-${month}-${day}`)
         cy.get('#status').should('have.value', '0')
-        cy.contains('test todo')
     })
 })
 
 //test delete todo
 describe('ToDo Anwendung', function () {
-    it('delete', function () {
+    it('Lösche ToDo', function () {
         cy.request({
             method: 'POST', 
             url: 'http://localhost:3000/todos',
             headers: headers,
             body: {
-                "title": "12345",
+                "title": "1234566666666666",
                 "due": "2023-11-14T00:00:00.000Z",
                 "status": 0
             }  
@@ -54,11 +73,73 @@ describe('ToDo Anwendung', function () {
             //get todo id
             const id = response.body._id
             cy.visit(`http://localhost:${PORT}/todo.html`)
+
+            //click delete button within div with id todo-id
             cy.get(`#todo-${id}`).within(()=>{
-                cy.get('.delete').click();
-                
+                cy.contains('Button','Löschen').click()
             });
-            cy.contains('#todo-${id}').should('not.exist');
+            cy.contains(`#todo-${id}`).should('not.exist');
+        })
+    })
+})
+
+//test edit todo
+describe('ToDo Anwendung', function () {
+    it('Bearbeite ToDo', function () {
+        cy.request({
+            method: 'POST', 
+            url: 'http://localhost:3000/todos',
+            headers: headers,
+            body: {
+                "title": "1234566666666666",
+                "due": "2023-11-14T00:00:00.000Z",
+                "status": 0
+            }  
+        }).then((response) => {
+            //get todo id
+            const id = response.body._id
+            cy.visit(`http://localhost:${PORT}/todo.html`)
+            cy.get(`#todo-${id}`)
+                .first()
+                .within(()=>{
+                    cy.get('.edit').click()
+                });
+
+            cy.get('#todo').clear().type('test todo edited')
+            cy.get('#due').clear().type('2023-11-14')
+            cy.get('#status').select('1')
+            cy.get('#add').click()
+            cy.get(`#todo-${id}`).contains('test todo edited')
+        })
+    })
+})
+
+//test change status of todo
+describe('ToDo Anwendung', function () {
+    it('Update Status', function () {
+        cy.request({
+            method: 'POST', 
+            url: 'http://localhost:3000/todos',
+            headers: headers,
+            body: {
+                "title": "1234566666666666",
+                "due": "2023-11-14T00:00:00.000Z",
+                "status": 0
+            }  
+        }).then((response) => {
+            //get todo id
+            const id = response.body._id
+            cy.visit(`http://localhost:${PORT}/todo.html`)
+            cy.get(`#todo-${id}`)
+                .first()
+                .within(()=>{
+                    cy.get('.edit').click()
+                    cy.get.contains('In Bearbeitung')
+                    cy.get('.edit').click()
+                    cy.get.contains('Erledigt')
+                    cy.get('.edit').click()
+                    cy.get.contains('Offen')
+                });
         })
     })
 })
