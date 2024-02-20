1e961e8da71d3a11350a44dfbd1c5c5c05574253
SBongardt <bongardt.sascha@fh-swf.de>
2024-02-20 17:16:43 +0100

    Token wird jetzt auch auf den richtigen Herausgeber überprüft

--- backend/index.js
+++ backend/index.js
@@ -21,6 +21,7 @@ const opts ={
     secretOrKey: `-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyn2vP592Ju/iKXQW1DCrSTXyQXyo11Qed1SdzFWC+mRtdgioKibzYMBt2MfAJa6YoyrVNgOtGvK659MjHALtotPQGmis1VVvBeMFdfh+zyFJi8NPqgBTXz6bQfnu85dbxVAg95J+1Ud0m4IUXME1ElOyp1pi88+w0C6ErVcFCyEDS3uAajBY6vBIuPrlokbl6RDcvR9zX85s+R/s7JeP1XV/e8gbnYgZwxcn/6+7moHPDl4LqvVDKnDq9n4W6561s8zzw8EoAwwYXUC3ZPe2/3DcUCh+zTF2nOy8HiN808CzqLq1VeD13q9DgkAmBWFNSaXb6vK6RIQ9+zr2cwdXiwIDAQAB
 -----END PUBLIC KEY-----`,
+    issuer: "https://jupiter.fh-swf.de/keycloak/realms/webentwicklung"
 }
 
 passport.use(new JwtStrategy(opts, (payload, done) => {
