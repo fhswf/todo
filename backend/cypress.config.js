451d20e0e6cebff6664c988474d5286604d41d18
HalleTim <halle.tim@fh-swf.de>
2024-02-27 22:36:39 +0100

    secure

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -6,12 +6,4 @@ export default defineConfig({
     e2e: {
         baseUrl: `http://localhost:${PORT}`,
     },
-    env: {
-        auth0_username: "public",
-        auth0_password: "todo",
-        auth0_domain: "https://jupiter.fh-swf.de/keycloak/realms/webentwicklung",
-        auth0_audience: "account",
-        auth0_scope: "openid email profile",
-        auth0_client_id: "todo-backend",
-      },
 })
