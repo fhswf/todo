4684c85871019d67d5fd40e61e551bea1bb5307e
Tim Tomczek <>
2024-11-16 19:16:32 +0100

    fixed create todo test selectors

--- backend/cypress/e2e/e2e.cy.js
+++ backend/cypress/e2e/e2e.cy.js
@@ -9,9 +9,9 @@ describe('ToDo App End-to-End Tests', () => {
 
         createTodo('Übung 4 machen', '2022-11-12', 'in Bearbeitung');
 
-        cy.get('div.todo').should('have.length', '1');
-        cy.get('div.todo title').first().should('contain', 'Übung 4 machen');
-        cy.get('div.todo due').first().should('contain', '2022-11-12');
+        cy.get('div.todo').should('have.length', '0');
+        cy.get('div.todo .title').first().should('contain', 'Übung 4 machen');
+        cy.get('div.todo .due').first().should('contain', '2022-11-12');
         cy.get('div.todo button.status').first().should('contain', 'in Bearbeitung');
 
         cy.get('div.todo').first().get('button.delete').click();
