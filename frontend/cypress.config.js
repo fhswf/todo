88647b623fc09c9f18c5adce2f64b86c6a5ee5d3
Robin Wessel <82476232+T3rr0rS0ck3@users.noreply.github.com>
2024-11-16 14:53:54 +0000

    cypres config angepasst

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,7 +2,13 @@ module.exports = {
   e2e: {
     videos: true,
     screenshotOnFailure: true,
-
+    reporter: 'mochawesome', // Optional: Mochawesome-Reporter für detaillierte Ergebnisse
+    reporterOptions: {
+      reportDir: 'cypress/results', // Ordner für Berichte
+      overwrite: false, // Verhindert das Überschreiben von bestehenden Berichten
+      html: false, // HTML-Berichte deaktivieren
+      json: true // JSON-Berichte aktivieren
+    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
