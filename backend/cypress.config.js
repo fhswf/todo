e70810bb4509ce0d55ad658fe9a22ce3100ae6cf
C.Peters <150918052+Ranea-D@users.noreply.github.com>
2024-11-20 12:20:09 +0000

    change

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 export default defineConfig({
     e2e: {
-        baseUrl: 'http://localhost:${PORT}',
+        baseUrl: `http://localhost:${PORT}`,
         supportFile: false,
         screenshotOnRunFailure: true,
         screenshotsFolder: 'cypress/screenshots',
