90eb5b7603971389826e5e91df6fd435dc8eadd3
Tim Tomczek <>
2024-11-20 19:45:28 +0100

    Added cypress support file to disable test failing on application error

--- backend/cypress.config.js
+++ backend/cypress.config.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 3000;
 export default defineConfig({
     e2e: {
         baseUrl: `http://localhost:${PORT}`,
-        supportFile: false,
         screenshotOnRunFailure: true,
         screenshotsFolder: 'cypress/screenshots',
         videosFolder: 'cypress/videos',
