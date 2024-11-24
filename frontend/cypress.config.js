0ad06d48ff6c43c86925b8bf33bf2a5eb9f8aa10
Robin Wessel <82476232+T3rr0rS0ck3@users.noreply.github.com>
2024-11-24 07:11:28 +0000

    Reporter korrgiert

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = {
     screenshotOnFailure: true,
     reporter: "cypress-sonarqube-reporter",
     reporterOptions: {
-      outputDir: "frontend/cypress/results"
+      outputDir: "cypress/results"
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
