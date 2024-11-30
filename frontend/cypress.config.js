e0a1534b9a1c5d7c08ebe2522734b8b98b45e2b6
Robin Wessel <82476232+T3rr0rS0ck3@users.noreply.github.com>
2024-11-30 10:13:20 +0000

    reporter entfernt

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -2,10 +2,6 @@ module.exports = {
   e2e: {
     videos: true,
     screenshotOnFailure: true,
-    reporter: "cypress-sonarqube-reporter",
-    reporterOptions: {
-      outputDir: "cypress/results"
-    },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
