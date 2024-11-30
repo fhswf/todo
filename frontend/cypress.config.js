341d11b894932fe207341a64836fba6161f00842
Robin Wessel <82476232+T3rr0rS0ck3@users.noreply.github.com>
2024-11-30 10:37:34 +0000

    workflow korrigiert und rpeorter hinzugefügt

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -4,6 +4,26 @@ module.exports = {
     screenshotOnFailure: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('after:run', (results) => {
+        const { merge } = require('mochawesome-merge')
+        const { create } = require('mochawesome-report-generator')
+
+        merge({
+          files: ['./cypress/results/*.json']
+        }).then(report => {
+          create(report, {
+            reportDir: './cypress/reports',
+            reportFilename: 'report.html'
+          })
+        })
+      })
     },
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true
+    }
   },
 };
