b7f6a67bad78c810759bb64be40dc504fca9bbe7
Tim Tomczek <>
2024-12-11 10:57:26 +0100

    removed cy.log to fillInForm

--- backend/cypress/e2e-test-utils.js
+++ backend/cypress/e2e-test-utils.js
@@ -1,5 +1,4 @@
 export function fillInForm(chain, name, duedate, status) {
-    cy.log('Filling in form with name: ' + name + ', due date: ' + duedate + ', status: ' + status);
     let statusNum;
     switch (status) {
         case 'offen':
