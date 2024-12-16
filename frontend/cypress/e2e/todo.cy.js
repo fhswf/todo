3c88c187d29ad53a8df6b69bccaf139f5b1e2ea4
Kevjas1301 <148619253+Kevjas1301@users.noreply.github.com>
2024-12-16 17:42:23 +0000

    frontend tests

--- frontend/cypress/e2e/todo.cy.js
+++ frontend/cypress/e2e/todo.cy.js
@@ -1,7 +1,49 @@
 const PORT = process.env.PORT || 3000;
 
 describe('Todo', function () {
+
+    beforeEach(() => {
+        cy.visit(`http://localhost:${PORT}/todo.html`);
+      });
+
     it('successfully loads', function () {
         cy.visit(`http://localhost:${PORT}/todo.html`) // ändern Sie URL zu Ihrer App
-    })
-})
+    });
+
+    it('fügt ein neues Todo hinzu und zeigt es an', function () {
+        cy.get('#todo').type('Test Todo');
+        cy.get('#due').type('2024-11-20');
+        cy.get('#status').select('offen');
+        cy.get('[type="submit"]').click();
+    
+        cy.get('#todo-list .todo').should('have.length', 1);
+        cy.contains('Neue Aufgabe');
+        cy.contains('2024-11-20');
+        cy.contains('offen');
+    });
+
+    it('zeigt eine Fehlermeldung, wenn kein Titel eingegeben wurde', function () {
+        cy.get('#due').type('2024-11-20');
+        cy.get('[type="submit"]').click();
+    
+        cy.contains('Bitte geben Sie einen Titel ein');
+    });
+
+    it('ändert den Status eines Todos', function () {
+        cy.get('#todo').type('Another Task');
+        cy.get('#due').type('2024-11-21');
+        cy.get('[type="submit"]').click();
+
+        cy.get('.status').click();
+        cy.contains('in Bearbeitung');
+    });
+
+    it('löscht ein Todo', function () {
+        cy.get('#todo').type('Task to Delete');
+        cy.get('#due').type('2024-11-22');
+        cy.get('[type="submit"]').click();
+
+        cy.get('.delete').click();
+        cy.get('#todo-list .todo').should('have.length', 0);
+    });
+});
--- frontend/cypress/support/e2e.js
+++ frontend/cypress/support/e2e.js
@@ -1,3 +1,9 @@
 beforeEach(() => {
     cy.log('I run before every test in every spec file!!!!!!')
 })
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // Return false, um Cypress mitzuteilen, die Exception zu ignorieren.
+    return false;
+  });
+  
